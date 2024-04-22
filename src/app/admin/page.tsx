9ba22c411ea9e07f48962a88ff65add1e48dbf7d
Mario Ballesteros <mariob360@gmail.com>
2024-04-22 13:33:26 -0700

    feat: get customer data
    - full count of how many customers
    - average value per customer

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,4 +1,4 @@
-// shadcn/ui 
+// shadcn/ui
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import db from '@/db/db';
 import { formatCurrency, formatNumber } from '@/lib/formatters';
@@ -24,6 +24,24 @@ async function getSalesData() {
   };
 }
 
+async function getCustomerData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: {
+        pricePaidInCents: true,
+      },
+    }),
+  ]);
+
+  const averageValuePerUser = userCount > 0 ? (orderData._sum.pricePaidInCents || 0) / userCount : 0;
+
+  return {
+    userCount,
+    averageValuePerUser,
+  };
+}
+
 const DashboardCard = (props: DashboardCardProps) => {
   const { title, subtitle, body } = props;
 
@@ -43,13 +61,25 @@ const DashboardCard = (props: DashboardCardProps) => {
 const AdminDashboard = async (props: AdminDashboardProps) => {
   // Fetch Data
   const [salesData, customerData] = await Promise.all([getSalesData(), getCustomerData()]);
+
+  // Data
   const { amount, numberOfSales } = salesData;
+  const { userCount, averageValuePerUser } = customerData;
+
   // Formatted Data
   const formattedAmount = formatCurrency(amount);
   const formattedNumberOfSales = formatNumber(numberOfSales);
+  const formattedAverageValuePerUser = formatCurrency(averageValuePerUser);
+  const formattedUserCount = formatNumber(userCount);
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4'>
       <DashboardCard title='Sales' subtitle={`${formattedNumberOfSales} Orders`} body={formattedAmount} />
+      <DashboardCard
+        title='Customers'
+        subtitle={`${formattedAverageValuePerUser} Average Value`}
+        body={formattedUserCount}
+      />
     </div>
   );
 };
