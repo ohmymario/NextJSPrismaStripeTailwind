7d8669aaaf1dc802816c9ee0a1247b94e9723cae
Mario Ballesteros <mariob360@gmail.com>
2024-04-29 16:27:53 -0700

    feature: server product submit
    - 'use server' directive
    - create schema for product
    - files are instance of File and if the file size is above 0
    - images are checked if image file and lower than 5mb
    - error handling is passed onto client component form
    - image and files are stored into products folder

--- src/app/admin/products/_actions/products.ts
+++ src/app/admin/products/_actions/products.ts
@@ -1,5 +1,60 @@
 "use server"
 
+import db from '@/db/db';
+import fs from 'fs/promises';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
+
+// Define a schema for the file to ensure it's an instance of File and refine for size
+const fileSchema = z
+  .instanceof(File, { message: "Required" }
+  );
+
+// Define a schema for the file to ensure it's an instance of File and refine for size
+const imageSchema = z
+  .instanceof(File, { message: "Required" })
+  .refine(file => file.type.startsWith('image/'), { message: 'File must be an image', })
+  .refine(file => file.size <= 1024 * 1024 * 5, { message: 'File size must be less than 5MB', }
+  );
+
+
+// Define a schema for the form data
+const addSchema = z.object({
+  name: z.string().min(2),
+  description: z.string().min(2),
+  priceInCents: z.coerce.number().int().positive().min(1),
+  file: fileSchema.refine(file => file.size > 0, "Required"),
+  image: imageSchema.refine(file => file.size > 0, "Required"),
+});
+
 export async function addProduct(formData: FormData) {
-  console.log('addProduct', formData);
+  const result = addSchema.safeParse(Object.fromEntries(formData.entries()));
+  if (result.success === false) {
+    console.log(result.error)
+    return result.error.formErrors.fieldErrors;
+  }
+
+  const data = result.data;
+
+  await fs.mkdir('products', { recursive: true });
+  const filePath = `products/${crypto.randomUUID()}-${data.file.name}`;
+  const fileBuffer = await data.file.arrayBuffer();
+  await fs.writeFile(filePath, Buffer.from(fileBuffer));
+
+  await fs.mkdir('public/products', { recursive: true });
+  const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
+  const imageBuffer = await data.image.arrayBuffer();
+  await fs.writeFile(`public${imagePath}`, Buffer.from(imageBuffer));
+
+  await db.product.create({
+    data: {
+      name: data.name,
+      description: data.description,
+      priceInCents: data.priceInCents,
+      filePath,
+      imagePath
+    },
+  });
+
+  redirect('/admin/products');
 }
