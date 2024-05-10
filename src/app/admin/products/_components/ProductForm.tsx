31384e2986b4ead011f542857dae686f8d41d55a
Mario Ballesteros <mariob360@gmail.com>
2024-05-09 19:41:54 -0700

    feat: editable product populates form

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -15,23 +15,29 @@ import { formatCurrency } from '@/lib/formatters';
 // actions
 import { addProduct } from '../_actions/products';
 import { Textarea } from '@/components/ui/textarea';
+import { Product } from '@prisma/client';
+import Image from 'next/image';
 
-interface ProductFormProps {}
+interface ProductFormProps {
+  product: Product | null;
+}
 
 export default function ProductForm(props: ProductFormProps) {
-  const [error, action] = useFormState(addProduct, {});
+  const { product } = props;
 
-  const [priceInCents, setPriceInCents] = useState<number>();
+  const [error, action] = useFormState(addProduct, {});
+  const [priceInCents, setPriceInCents] = useState<number | undefined>(product?.priceInCents);
 
   return (
     <form className='space-y-8' action={action}>
+      {/* Name */}
       <div className='space-y-2'>
         <Label htmlFor='name'>Name</Label>
-        <Input type='text' id='name' name='name' required />
-
+        <Input type='text' id='name' name='name' required defaultValue={product?.name || ''} />
         {error.name && <div className='text-destructive'>{error.name}</div>}
       </div>
 
+      {/* Price */}
       <div>
         <div className='space-y-2'>
           <Label htmlFor='priceInCents'>Price in Cents</Label>
@@ -44,28 +50,36 @@ export default function ProductForm(props: ProductFormProps) {
             onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
           />
         </div>
-
         <div className='text-muted-foreground'>{formatCurrency((priceInCents || 0) / 100)}</div>
-
         {error.priceInCents && <div className='text-destructive'>{error.priceInCents}</div>}
       </div>
 
+      {/* Description */}
       <div className='space-y-2'>
         <Label htmlFor='description'>Description</Label>
-
-        <Textarea id='description' name='description' required />
+        <Textarea id='description' name='description' required defaultValue={product?.description} />
         {error.description && <div className='text-destructive'>{error.description}</div>}
       </div>
 
+      {/* File */}
       <div className='space-y-2'>
         <Label htmlFor='file'>File</Label>
-        <Input type='file' id='file' name='file' required />
+        <Input type='file' id='file' name='file' required={product === null} />
+
+        {/* File Path */}
+        {product != null && <div className='text-muted-foreground'>{product.filePath}</div>}
+
         {error.file && <div className='text-destructive'>{error.file}</div>}
       </div>
 
+      {/* Image */}
       <div className='space-y-2'>
         <Label htmlFor='image'>Image</Label>
-        <Input type='file' id='image' name='image' required />
+        <Input type='file' id='image' name='image' required={product === null} />
+
+        {/* File Path */}
+        {product != null && <Image src={product.imagePath} width={400} height={400} alt={product.name} />}
+
         {error.image && <div className='text-destructive'>{error.image}</div>}
       </div>
 
