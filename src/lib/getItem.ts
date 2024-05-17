42768090bbb192ec78124e0e72bcf0279f915ceb
Mario Ballesteros <mariob360@gmail.com>
2024-05-17 14:15:22 -0700

    fix: remove unnecessary await

--- src/lib/getItem.ts
+++ src/lib/getItem.ts
@@ -2,7 +2,7 @@ import { cache } from 'react'
 import db from '@/db/db'
 
 export const getItem = cache(async (id: string) => {
-  const product = await await db.product.findUnique({
+  const product = await db.product.findUnique({
     where: {
       id,
     },
