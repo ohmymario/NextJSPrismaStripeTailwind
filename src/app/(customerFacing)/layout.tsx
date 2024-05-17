39a96b2b5441af9f73d73f67d7adafda67bde41b
Mario Ballesteros <mariob360@gmail.com>
2024-05-17 16:34:24 -0700

    feat: create new user homepage
    - copy layout from admin route
    - keeps most recent data with 'force-dynamic'
    -  Update links to relevant for user

---
+++ src/app/(customerFacing)/layout.tsx
@@ -0,0 +1,23 @@
+import { Nav, NavLink } from '@/components/Nav';
+
+// No Cached Data, Keep Data Fresh
+export const dynamic = 'force-dynamic';
+
+const Layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <>
+      <Nav>
+        <NavLink href='/'>Home</NavLink>
+        <NavLink href='/products'>Products</NavLink>
+        <NavLink href='/orders'>My Orders</NavLink>
+      </Nav>
+      <div className='container my-6'>{children}</div>
+    </>
+  );
+};
+
+export default Layout;
