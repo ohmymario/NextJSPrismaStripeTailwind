ade1592bb1098e046d5e0fdba095467b06258793
Mario Ballesteros <mariob360@gmail.com>
2024-05-22 15:12:30 -0700

    feat: product card skeleton
    - create jsx for loading state
    - visual to let user know loading it occuring

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,18 +5,6 @@ import { Button } from './ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
 import Image from 'next/image';
 
-// (parameter) product: {
-//     id: string;
-//     name: string;
-//     priceInCents: number;
-//     filePath: string;
-//     imagePath: string;
-//     description: string;
-//     isAvailableForPurchase: boolean;
-//     createdAt: Date;
-//     updatedAt: Date;
-// }
-
 export function ProductCard({ product }: { product: Product }) {
   const { name, description, priceInCents, id, imagePath } = product;
   const formattedCurrency = formatCurrency(priceInCents / 100);
@@ -48,5 +36,29 @@ export function ProductCard({ product }: { product: Product }) {
         </CardFooter>
       </Card>
     </div>
+  );
+}
+
+export function ProductCardSkeleton() {
+  return (
+    <Card className='overflow-hidden flex flex-col animate-pulse'>
+      <div className='w-full aspect-video bg-gray-300' />
+      <CardHeader>
+        <CardTitle>
+          <div className='w-3/4 h-6 rounded-full bg-gray-300' />
+        </CardTitle>
+        <CardDescription>
+          <div className='w-1/2 h-4 rounded-full bg-gray-300' />
+        </CardDescription>
+      </CardHeader>
+      <CardContent className='space-y-2'>
+        <div className='w-full h-4 rounded-full bg-gray-300' />
+        <div className='w-full h-4 rounded-full bg-gray-300' />
+        <div className='w-3/4 h-4 rounded-full bg-gray-300' />
+      </CardContent>
+      <CardFooter>
+        <Button className='w-full' disabled size='lg'></Button>
+      </CardFooter>
+    </Card>
   );
 }
