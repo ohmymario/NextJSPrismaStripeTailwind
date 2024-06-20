4be5c921763611e08dd640270c025e7cc967ade9
Mario Ballesteros <mariob360@gmail.com>
2024-06-20 15:50:53 -0700

    feat(checkout): add base stripe integration

---
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -0,0 +1,31 @@
+import db from '@/db/db';
+import { notFound } from 'next/navigation';
+import Stripe from 'stripe';
+import CheckoutForm from './_components/CheckoutForm';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+interface PurchasePageProps {
+  params: { id: string };
+}
+
+export default async function PurchasePage({ params: { id } }: PurchasePageProps) {
+  const product = await db.product.findUnique({ where: { id } });
+
+  // check for product
+  if (product === null) return notFound();
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: 'USD',
+    metadata: { product: product.id },
+  });
+
+  if (paymentIntent.client_secret === null) {
+    throw new Error('Stripe failed to create payment intent.');
+  }
+
+  const { client_secret } = paymentIntent;
+
+  return <CheckoutForm product={product} clientSecret={client_secret} />;
+}
