074287f6b7424473b9282850d2955f8e1644d457
Mario Ballesteros <mariob360@gmail.com>
2024-06-28 16:39:44 -0700

    feat(strip): create stripe success component
    - checks for payment from stripe
    - uses id to find product in prisma database
    - found product will populate jsx to display product purchased to user

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -1,5 +1,41 @@
-interface PageProps {}
+import db from '@/db/db';
+import { formatCurrency } from '@/lib/formatters';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+import Stripe from 'stripe';
 
-export default function Page(props: PageProps) {
-  return <h1>Hi</h1>;
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+interface SuccessPageProps {
+  searchParams: { payment_intent: string };
+}
+
+export default async function SuccessPage({ searchParams }: SuccessPageProps) {
+  const paymentIntent = await stripe.paymentIntents.retrieve(searchParams.payment_intent);
+  const stripeProduct = paymentIntent.metadata.productId;
+
+  if (!stripeProduct) return notFound();
+
+  const product = await db.product.findUnique({ where: { id: stripeProduct } });
+
+  if (!product) return notFound();
+
+  const isSuccess = paymentIntent.status === 'succeeded';
+
+  return (
+    <div className='max-w-5xl w-full mx-auto space-y-8'>
+      <h1>{isSuccess ? 'Successful' : 'failed'}</h1>
+
+      <div className='flex gap-4 items-center'>
+        <div className='aspect-video flex-shrink-0 w-1/3 relative'>
+          <Image src={product.imagePath} fill alt={product.name} className='object-cover' />
+        </div>
+        <div>
+          <div className='text-lg '>{formatCurrency(product.priceInCents / 100)}</div>
+          <h1 className='text-2xl font-bold'>{product.name}</h1>
+          <div className='line-clamp-3 text-muted-foreground'>{product.description}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
