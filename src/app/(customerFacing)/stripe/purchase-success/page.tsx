8d8e8701541658fb7a785a8df69a228e45556844
Mario Ballesteros <mariob360@gmail.com>
2024-07-03 12:24:48 -0700

    feat(download): link
    - give user paid for download link
    - payment failure will allow user to try again
    - 24 hour link

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -24,7 +24,7 @@ export default async function SuccessPage({ searchParams }: SuccessPageProps) {
 
   return (
     <div className='max-w-5xl w-full mx-auto space-y-8'>
-      <h1>{isSuccess ? 'Successful' : 'failed'}</h1>
+      <h1 className='4xl font-bold'>{isSuccess ? 'Successful!' : 'Error!'}</h1>
 
       <div className='flex gap-4 items-center'>
         <div className='aspect-video flex-shrink-0 w-1/3 relative'>
@@ -35,7 +35,27 @@ export default async function SuccessPage({ searchParams }: SuccessPageProps) {
           <h1 className='text-2xl font-bold'>{product.name}</h1>
           <div className='line-clamp-3 text-muted-foreground'>{product.description}</div>
         </div>
+
+        <Button className='mt-4' size='lg' asChild>
+          {isSuccess ? (
+            <a href={`/products/download/${await createDownloadVerification(product.id)}`}>Download</a>
+          ) : (
+            <a href={`/products/${product.id}/purchase`}>Try Again</a>
+          )}
+        </Button>
       </div>
     </div>
   );
+}
+
+async function createDownloadVerification(productId: string) {
+  return (
+    await db.downloadVerifications.create({
+      data: {
+        productId,
+        // 24 hours
+        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
+      },
+    })
+  ).id;
 }
