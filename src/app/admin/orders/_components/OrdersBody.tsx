9b6755829cafd90fa94bcbf89b6ac97a65b21709
Mario Ballesteros <mariob360@gmail.com>
2024-07-15 17:24:59 -0700

    feat(order): introduce body for orders table
    - allows admin to view all orders
    - delete single orders using menu

---
+++ src/app/admin/orders/_components/OrdersBody.tsx
@@ -0,0 +1,41 @@
+import { TableBody, TableCell, TableRow } from '@/components/ui/table';
+import { formatCurrency } from '@/lib/formatters';
+import { MoreVertical } from 'lucide-react';
+import { DeleteDropDownItem } from './OrderActions';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+
+interface OrdersBodyProps {
+  orders: {
+    id: string;
+    pricePaidInCents: number;
+    product: { name: string };
+    user: { email: string };
+  }[];
+}
+
+export default function OrdersBody({ orders }: OrdersBodyProps) {
+  return (
+    <TableBody>
+      {orders.map((order) => (
+        <TableRow key={order.id}>
+          <TableCell>{order.product.name}</TableCell>
+          <TableCell>{order.user.email}</TableCell>
+          <TableCell>{formatCurrency(order.pricePaidInCents / 100)}</TableCell>
+
+          {/* MENU */}
+          <TableCell className='text-center'>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <MoreVertical />
+                <span className='sr-only'>Actions</span>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DeleteDropDownItem id={order.id} />
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  );
+}
