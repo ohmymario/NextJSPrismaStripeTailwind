1089dc32a492c020ce04a0915a1b8c6d8f88c6a1
Mario Ballesteros <mariob360@gmail.com>
2024-09-12 15:50:37 -0700

    feat(helpers): create formatDiscountType function
    - converts the amount to the correct type
    - percentage formatted with formatPercentage
    - fixed formatted with formatCurrency

--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -1,3 +1,7 @@
+import { DiscountCodeType } from "@prisma/client";
+
+const NUMBER_FORMATTER = new Intl.NumberFormat('en-US');
+const PERCENT_FORMATTER = new Intl.NumberFormat('en-US', { style: 'percent' });
 const CURRENCY_FORMATTER = new Intl.NumberFormat('en-US', {
   currency: 'USD',
   style: 'currency',
@@ -8,8 +12,22 @@ export function formatCurrency(amount: number) {
   return CURRENCY_FORMATTER.format(amount);
 }
 
-const NUMBER_FORMATTER = new Intl.NumberFormat('en-US');
-
 export function formatNumber(amount: number) {
   return NUMBER_FORMATTER.format(amount);
 }
+
+export function formatPercentage(amount: number) {
+  return PERCENT_FORMATTER.format(amount / 100);
+}
+
+export function formatDiscountType({ discountAmount, discountType }: { discountAmount: number, discountType: DiscountCodeType }) {
+  if (discountType === 'PERCENTAGE') {
+    return formatPercentage(discountAmount);
+  }
+
+  if (discountType === 'FIXED') {
+    return formatCurrency(discountAmount);
+  }
+
+  return new Error(`Unknown discount type: ${discountType satisfies never}`);
+} 
