81e63c27918b39172bf0a17131bb69963612bb59
Mario Ballesteros <mariob360@gmail.com>
2024-09-25 14:24:02 -0700

    feat(coupon): pass all products to coupon form
    - query database to obtain id and name of all products
    - order query by name
    - pass all products into DiscountCodeForm

--- src/app/admin/discount-codes/new/page.tsx
+++ src/app/admin/discount-codes/new/page.tsx
@@ -1,13 +1,24 @@
+import db from '@/db/db';
 import PageHeader from '../../_components/PageHeader';
 import DiscountCodeForm from '../_components/DiscountCodeForm';
 
 interface NewDiscountCodePageProps {}
 
-export default function NewDiscountCodePage(props: NewDiscountCodePageProps) {
+export default async function NewDiscountCodePage(props: NewDiscountCodePageProps) {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      name: true,
+    },
+    orderBy: { name: 'asc' },
+  });
+
+  console.log(products);
+
   return (
     <>
       <PageHeader>Add DiscountCode</PageHeader>
-      <DiscountCodeForm />
+      <DiscountCodeForm products={products} />
     </>
   );
 }
