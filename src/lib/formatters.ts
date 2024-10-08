8dbfa790b8596b1409ade5b092c18f1ce8128f81
Mario Ballesteros <mariob360@gmail.com>
2024-10-08 16:14:06 -0700

    chore: update formatters
    - add time formatter
    - update percent formatter with pretier

--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -1,11 +1,17 @@
 import { DiscountCodeType } from "@prisma/client";
 
 const NUMBER_FORMATTER = new Intl.NumberFormat('en-US');
-const PERCENT_FORMATTER = new Intl.NumberFormat('en-US', { style: 'percent' });
+const PERCENT_FORMATTER = new Intl.NumberFormat('en-US',
+  { style: 'percent' }
+);
 const CURRENCY_FORMATTER = new Intl.NumberFormat('en-US', {
   currency: 'USD',
   style: 'currency',
   minimumFractionDigits: 0,
+});
+const DATE_TIME_FORMATTER = new Intl.DateTimeFormat('en-US', {
+  dateStyle: 'medium',
+  timeStyle: 'short',
 });
 
 export function formatCurrency(amount: number) {
@@ -30,4 +36,8 @@ export function formatDiscountType({ discountAmount, discountType }: { discountA
   }
 
   return new Error(`Unknown discount type: ${discountType satisfies never}`);
-} 
+}
+
+export function formatDateTime(date: Date) {
+  return DATE_TIME_FORMATTER.format(date);
+}
