2a85cde5526f62dc851a923fbc82019e52b7901c
Mario Ballesteros <mariob360@gmail.com>
2024-10-10 16:24:51 -0700

    refactor(coupon): move discount code table
    - simplify main discount-code
    - move imports, jsx

--- src/app/admin/discount-codes/page.tsx
+++ src/app/admin/discount-codes/page.tsx
@@ -1,52 +1,15 @@
 import db from '@/db/db';
-import { $Enums, Prisma, Product } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 import Link from 'next/link';
 
 // Components
 import PageHeader from '../_components/PageHeader';
+import { DiscountCodesTable } from './_components/DiscountCodeTable';
 
 // Shadcn
 import { Button } from '@/components/ui/button';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-
-// Formatters
-import { formatDateTime, formatDiscountType, formatNumber } from '@/lib/formatters';
-import { CheckCircle2, Globe, Infinity, Minus, MoreVertical, XCircle } from 'lucide-react';
 
 interface DiscountCodesProps {}
-
-interface DiscountCodesTableProps {
-  discountCodes: Awaited<
-    Prisma.PrismaPromise<
-      {
-        id: string;
-        code: string;
-        discountAmount: number;
-        discountType: $Enums.DiscountCodeType;
-        uses: number;
-        isActive: boolean;
-        allProducts: boolean;
-        createdAt: Date;
-        limit: number | null;
-        expiresAt: Date | null;
-        _count: {
-          orders: number;
-        };
-        products: {
-          id: string;
-          name: string;
-        }[];
-      }[]
-    >
-  >;
-}
 
 const WHERE_EXPIRED: Prisma.DiscountCodeWhereInput = {
   OR: [
@@ -101,100 +64,12 @@ export default async function DiscountCodes(props: DiscountCodesProps) {
           <Link href='/admin/discount-codes/new'>Add Coupon</Link>
         </Button>
       </div>
-      <DiscountCodesTable discountCodes={activeDiscountCodes} />
+      <DiscountCodesTable discountCodes={activeDiscountCodes} canDeactivate />
 
       <div>
         <h2 className='text-xl font-bold'>Expired Coupons</h2>
-        <DiscountCodesTable discountCodes={expiredDiscountCodes} />
+        <DiscountCodesTable discountCodes={expiredDiscountCodes} isInactive />
       </div>
     </>
-  );
-}
-
-function DiscountCodesTable({ discountCodes }: DiscountCodesTableProps) {
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className='w-0'>
-            <span className='sr-only'>Activated Discount Codes</span>
-          </TableHead>
-          <TableHead>Code</TableHead>
-          <TableHead>Discount</TableHead>
-          <TableHead>Expires</TableHead>
-          <TableHead>Remaining Uses</TableHead>
-          <TableHead>Orders</TableHead>
-          <TableHead>Products</TableHead>
-          <TableHead className='w-0'>
-            <span className='sr-only'>Deleting</span>
-            <span className='sr-only'>Activate</span>
-            <span className='sr-only'>Actions</span>
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-
-      <TableBody>
-        {discountCodes.map(
-          ({
-            id,
-            code,
-            discountAmount,
-            discountType,
-            isActive,
-            expiresAt,
-            limit,
-            uses,
-            _count,
-            products,
-            allProducts,
-          }) => (
-            <TableRow key={id}>
-              {/* Discount Code Status */}
-              <TableCell className='w-0'>
-                {isActive ? (
-                  <>
-                    <span className='sr-only'>Active</span>
-                    <CheckCircle2 />
-                  </>
-                ) : (
-                  <>
-                    <span className='sr-only'>Inactive</span>
-                    <XCircle className='stroke-destructive' />
-                  </>
-                )}
-              </TableCell>
-              <TableCell>{code}</TableCell>
-              <TableCell>{formatDiscountType({ discountAmount, discountType }) as string}</TableCell>
-              <TableCell>{expiresAt === null ? <Minus /> : formatDateTime(expiresAt)}</TableCell>
-              <TableCell>{limit === null ? <Infinity /> : `${limit - uses}`}</TableCell>
-              <TableCell>{formatNumber(_count.orders)}</TableCell>
-              <TableCell>{allProducts ? <Globe /> : products.map(({ name }) => name).join(', ')}</TableCell>
-
-              {/* Discount Code Actions */}
-              <TableCell>
-                <DropdownMenu>
-                  <DropdownMenuTrigger>
-                    <MoreVertical />
-                    <span className='sr-only'>Actions</span>
-                  </DropdownMenuTrigger>
-
-                  {/* Dropdown menu inside */}
-                  <DropdownMenuContent>
-                    {/* <ActiveToggleDropdownItem
-                    id={id}
-                    isAvailableForPurchase={isAvailableForPurchase}
-                  /> */}
-
-                    <DropdownMenuSeparator />
-
-                    {/* <DeleteDropdownItem id={id} disabled={_count.orders > 0} /> */}
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
-            </TableRow>
-          )
-        )}
-      </TableBody>
-    </Table>
   );
 }
