fe912f83a5aa3294bcb11eca15954b3c8ad8f906
Mario Ballesteros <mariob360@gmail.com>
2024-11-07 13:41:50 -0800

    feat(checkout): display discounted price
    - checkout now displays old and updated price
    - tailwind for styling line through

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -34,6 +34,12 @@ interface CheckoutFormProps {
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string);
 
 export default function CheckoutForm({ product, clientSecret, discountCode }: CheckoutFormProps) {
+  const adjustedPriceInCents = !discountCode
+    ? product.priceInCents
+    : getDiscountedAmount(product.priceInCents, discountCode);
+
+  const discountFound = product.priceInCents !== adjustedPriceInCents;
+
   return (
     <div className='max-w-5xl w-full mx-auto space-y-8'>
       <div className='flex gap-4 items-center'>
@@ -41,12 +47,30 @@ export default function CheckoutForm({ product, clientSecret, discountCode }: Ch
           <Image src={product.imagePath} fill alt={product.name} className='object-cover' />
         </div>
         <div>
-          <div className='text-lg '>{formatCurrency(product.priceInCents / 100)}</div>
+          <div className='text-lg flex gap-4 items-baseline'>
+            {/* Original price crossed out if discount is found */}
+            <div className={`${discountFound ? 'line-through text-muted-foreground' : ''}`}>
+              {formatCurrency(product.priceInCents / 100)}
+            </div>
+
+            {/* Price */}
+            {discountFound && <div>{formatCurrency(adjustedPriceInCents / 100)}</div>}
+          </div>
+
+          {/* Product name and description */}
           <h1 className='text-2xl font-bold'>{product.name}</h1>
           <div className='line-clamp-3 text-muted-foreground'>{product.description}</div>
         </div>
       </div>
 
+      <Elements
+        options={{
+          amount: adjustedPriceInCents,
+          mode: 'payment',
+          currency: 'usd',
+        }}
+        stripe={stripePromise}
+      >
         <Form priceInCents={adjustedPriceInCents} productId={product.id} discountCode={discountCode} />
       </Elements>
     </div>
