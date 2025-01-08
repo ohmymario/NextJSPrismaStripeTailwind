df4d21a9f13a2634ece9a8d3d9a2f9d55c11b644
Mario Ballesteros <mariob360@gmail.com>
2025-01-07 21:20:54 -0800

    feat(preview): create base modal
    - User can view their item before posting listing

---
+++ src/app/admin/_components/PreviewModal.tsx
@@ -0,0 +1,61 @@
+// shadcn
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
+// components
+import { ProductCard } from '@/components/ProductCard';
+
+// icons
+import { Eye } from 'lucide-react';
+
+interface PreviewModalProps {
+  product: {
+    id: string;
+    name: string;
+    description: string;
+    priceInCents: number;
+    imagePath: string;
+    isAvailableForPurchase: boolean;
+  };
+}
+
+const PreviewModal = ({ product }: PreviewModalProps) => {
+  return (
+    <Dialog>
+      {/* VISIBLE BUTTON */}
+      <DialogTrigger asChild>
+        <Button variant='outline' type='button' className='gap-2'>
+          <Eye className='h-4 w-4' />
+          Preview
+        </Button>
+      </DialogTrigger>
+
+      {/* ACTUAL MODAL CONTENT */}
+      <DialogContent className='max-w-2xl'>
+        <DialogHeader>
+          <DialogTitle>Product Preview</DialogTitle>
+        </DialogHeader>
+        {/* TODO: Add ProductCard component w/ preview prop */}
+        {/* <ProductCard product={product} preview /> */}
+
+        {/* TEMPORARY */}
+        <DialogDescription>
+          <p>{product.name}</p>
+          <p>{product.description}</p>
+          <p>${product.priceInCents}</p>
+          <p>{product.imagePath}</p>
+          <p>{product.isAvailableForPurchase}</p>
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default PreviewModal;
